57151351f0ac327483483f4e705103e766de08de
WisdomLLong <G66601828>
2017-05-18 19:12:08 +0800

    naive_bayes v1.2
    从网络端下载数据，如何避免下次运行程序时重新下载。
    保存数据到本地，判断路径文件来判断是否已经下载。

--- naive_bayes/naive_bayes.py
+++ naive_bayes/naive_bayes.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# 对新闻文本进行分类处理
+
 ## from后面的一句话中，最后一个点前面的都是文件夹，后面的是一个py文件。
 ## import的是一个类，创建实体后，就可以运用其中的方法了
 ## import的或是一个方法，不用创建实体，可以直接用
-
-# 对新闻文本进行分类处理
 
 from sklearn.datasets import fetch_20newsgroups
 #与之前预测的数据不同，fetch_20newsgroups需要即时从互联网下载数据
@@ -14,9 +14,9 @@ from sklearn.datasets import fetch_20newsgroups
 #if already == 0:
 #    print('*********************\n已经下载好新闻的数据了\n*********************')
 #else:
-    news = fetch_20newsgroups(subset='all')   #unexpected intend 非理想缩进
+news = fetch_20newsgroups(subset='all')   #unexpected indent 非理想缩进
     # 如何将news保存到本地？？？这样就可以根据路径和名字判断是否已经下载文件了
-    already = 0
+already = 0
     
 print(len(news.data))
 print(news.data[0])
