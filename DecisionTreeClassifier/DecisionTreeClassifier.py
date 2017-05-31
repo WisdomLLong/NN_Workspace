f7423ffdea5a4bedf39dd7dcd3b87edda65ed088
WisdomLLong <f_l_wang@163.com>
2017-05-31 21:22:31 +0800

    DecisionTree v1.1
    Finish

--- DecisionTreeClassifier/DecisionTreeClassifier.py
+++ DecisionTreeClassifier/DecisionTreeClassifier.py
@@ -6,9 +6,9 @@ import pandas as pd
 titanic = pd.read_csv('http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic.txt')
 print(titanic.head())
 
-#--------------------------
+#------------------------------------------------------------------------------
 # 数据预处理
-#--------------------------
+#------------------------------------------------------------------------------
 # 使用pandas，数据转入pandas独有的dataframe格式（二维数据表格），直接使用
 # info()，查看数据的统计特性，仅仅是查看
 titanic.info()
@@ -30,7 +30,38 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, \
                             random_state=33)
 
+# 特征转换器，特征提取
 from sklearn.feature_extraction import DictVectorizer
 vec = DictVectorizer(sparse=False)
+# 转换特征后，我们发现凡是类别型的特征都单独玻璃出来，独成一列特征，数值型的则保持不变。
+X_train = vec.fit_transform(X_train.to_dict(orient='record'))
+
+# 同样需要对测试数据进行转换
+X_test = vec.transform(X_test.to_dict(orient='record'))
+
+#------------------------------------------------------------------------------
+# 使用决策树分类器
+#------------------------------------------------------------------------------
+from sklearn.tree import DecisionTreeClassifier
+# 默认配置初始化决策树
+dtc = DecisionTreeClassifier()
+# 训练
+dtc.fit(X_train, y_train)
+# 预测
+y_predict = dtc.predict(X_test)
+
+
+#------------------------------------------------------------------------------
+# 预测性能评估
+#------------------------------------------------------------------------------
+from sklearn.metrics import classification_report
+print( dtc.score(X_test, y_test))
+print( classification_report(y_predict, y_test, target_names=['died', 'survived']))
+
+
+
+
+
+
 
 
