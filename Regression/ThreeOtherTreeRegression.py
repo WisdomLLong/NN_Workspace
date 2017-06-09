71eca255a7030c7c7b2f6bb6f30d134adaac1f3c
WisdomLLong <f_l_wang@163.com>
2017-06-09 11:35:09 +0800

    树回归的三个变种，性能比较优秀

---
+++ Regression/ThreeOtherTreeRegression.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# 美国波士顿地区房价数据描述
+
+# 读取数据
+from sklearn.datasets import load_boston
+boston = load_boston()
+print(boston.DESCR)
+
+# 数据分隔
+from sklearn.cross_validation import train_test_split
+import numpy as np
+
+X = boston.data
+y = boston.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33, \
+                    test_size = 0.25)
+'''
+print("The max target value is", np.max(y))
+print("The min target value is", np.min(y))
+print("The average target value is", np.mean(y))
+'''
+
+#------------------------------------------------------------------------------
+# 数据标准化处理
+#------------------------------------------------------------------------------
+from sklearn.preprocessing import StandardScaler
+# 分别初始化对特征和目标值的标准化
+ss_X = StandardScaler()
+ss_y = StandardScaler()
+
+X_train = ss_X.fit_transform(X_train)
+X_test = ss_X.transform(X_test)
+y_train = ss_y.fit_transform(y_train)
+y_test = ss_y.transform(y_test)
+
+
+#------------------------------------------------------------------------------
+# 三种树回归的改进
+#------------------------------------------------------------------------------
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor, GradientBoostingRegressor
+
+rfr = RandomForestRegressor()
+rfr.fit(X_train, y_train)
+rfr_y_predict = rfr.predict(X_test)
+
+etr = ExtraTreesRegressor()
+etr.fit(X_train, y_train)
+etr_y_predict = etr.predict(X_test)
+
+gbr = GradientBoostingRegressor()
+gbr.fit(X_train, y_train)
+gbr_y_predict = gbr.predict(X_test)
+
+
+#------------------------------------------------------------------------------
+# 三种方法进行性能预测，这里不再赘述，与线性回归中的相同
+#------------------------------------------------------------------------------
